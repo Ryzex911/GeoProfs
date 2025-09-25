08bcc0baa80d3f49dea5c7f8915fa0e1362fb849
Lukas-Greef <1209628@student.roc-nijmegen.nl>
2025-09-25 13:08:27 +0200

    push

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,6 +18,7 @@
                 }
             })();
         </script>
+        //
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
