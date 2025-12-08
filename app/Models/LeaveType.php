8a4097909806673a3f8e45be2c19ac0d9d9ffef7
Osama Alasmi <153827554+osama0210@users.noreply.github.com>
2025-12-08 19:05:52 +0100

    refactor(US005): update LeaveRequest factory and models for new ERD

---
+++ app/Models/LeaveType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class LeaveType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'requires_proof',
+        'max_days',
+    ];
+
+    protected $casts = [
+        'requires_proof' => 'boolean',
+        'max_days' => 'integer',
+    ];
+
+    public function leaveRequests(): HasMany
+    {
+        return $this->hasMany(LeaveRequest::class, 'leave_type_id');
+    }
+}
