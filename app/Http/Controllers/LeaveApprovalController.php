f1bc2fc7011092bf285221416b51b244e861e272
Osama Alasmi <153827554+osama0210@users.noreply.github.com>
2026-01-08 14:02:01 +0100

    chore(US011): add LeaveApprovalController skeleton for manager actions

---
+++ app/Http/Controllers/LeaveApprovalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LeaveApprovalController extends Controller
+{
+    public function index()
+    {
+        // todo: lijst met verlofaanvragen
+    }
+
+    public function approve()
+    {
+        // todo: goedkeuren
+    }
+
+    public function reject()
+    {
+        // todo: afwijzen
+    }
+}
